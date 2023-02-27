61248b6468b6abaf864ddf39ee6106d3a4eae1c2
larrymutoni <92687695+larrymutoni@users.noreply.github.com>
2023-02-27 18:09:52 +0300

    commit 1

---
+++ app/Http/Controllers/ContactController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Contact;
+class ContactController extends Controller
+{
+    public function contacts(){
+        $contacts = Contact::all();
+        return response()->json(
+            [
+                'contacts' => $contacts,
+                'message' => 'Contacts',
+                'code' => 200
+            ]
+        );
+    }
+
+    public function saveContact(Request $request){
+        $contact = new Contact();
+        $contact->name = $request->name;
+        $contact->email = $request->email;
+        $contact->designation = $request->designation;
+        $contact->contact_no = $request->contact_no;
+
+        $contact->save();
+
+        return response()->json(
+            [
+                'message' => 'Contact Created Successfully',
+                'code' => 200
+            ]
+        );
+    }
+}
